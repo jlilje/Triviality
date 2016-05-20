9a0a820430a7ce5e197b7df40cf1a03b97bb2b07
Jeremy Lilje <lilje55junk@hotmail.com>
2016-05-20 13:19:02 -0400

    Initial Commit

---
+++ Day5-TriviaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day5-TriviaApp
+//
+//  Created by Jeremy Lilje on 5/20/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Day5-TriviaApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Day5-TriviaApp
+//
+//  Created by Jeremy Lilje on 5/20/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
